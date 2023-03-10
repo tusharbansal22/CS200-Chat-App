4a0efe5b4e53d2efb314ff182b9cf0f2bfb70d8d
tusharb <tusharbansal@iitbhilai.ac.in>
2023-03-10 18:33:38 +0530

    added text field

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -10,6 +10,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   @override
+  String message="";
   final supabase = Supabase.instance.client;
   String email="";
   Future _getData() async{
@@ -35,28 +36,48 @@ class _ChatScreenState extends State<ChatScreen> {
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
             List<Widget> children;
             if (snapshot.hasData) {
+              print(snapshot.data.length);
               children = <Widget>[
                 ListView.builder(
+                  reverse: true,
                     scrollDirection: Axis.vertical,
                     shrinkWrap: true,
                     itemCount:(snapshot.data).length,
                     itemBuilder: (BuildContext context, int index) {
                       return Column(
-                          crossAxisAlignment: (snapshot.data[index]['user']=='email')?CrossAxisAlignment.start:CrossAxisAlignment.end,
+                          crossAxisAlignment: (snapshot.data[(snapshot.data).length-index-1]['user']!='email')?CrossAxisAlignment.start:CrossAxisAlignment.end,
                           children: [
                             Container(
                                 margin:EdgeInsets.symmetric(horizontal: 15,vertical: 2),
-                                child: Text('${snapshot.data[index]['user']}',style: TextStyle(fontSize: 12),)),
+                                child: Text('${snapshot.data[(snapshot.data).length-index-1]['user']}',style: TextStyle(fontSize: 12),)),
                             Container(
-                              width: 200,
+                              constraints: BoxConstraints(minWidth: 80, maxWidth: 200),
                               margin:EdgeInsets.symmetric(horizontal: 15,vertical: 2),
                               padding:EdgeInsets.symmetric(horizontal: 5,vertical: 20),
-                              decoration:BoxDecoration(color: (snapshot.data[index]['user']=='email')?Colors.blue.shade700:Colors.blueGrey.shade400,borderRadius: BorderRadius.circular(10)),
-                              child: Text('${snapshot.data[index]['msg']}'),
+                              decoration:BoxDecoration(color: (snapshot.data[(snapshot.data).length-index-1]['user']!='email')?Colors.blue.shade900:Colors.blueGrey.shade400,borderRadius: BorderRadius.circular(10)),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                child: Text('${snapshot.data[(snapshot.data).length-index-1]['msg']}',style: TextStyle(color: (snapshot.data[(snapshot.data).length-index-1]['user']!='email')?Colors.white:Colors.black),),
+                              ),
                             )
                           ],
                         );
                     }
+                ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 250,
+                        child: TextField(onChanged: (change){
+                          message=change;
+                        },),
+                      ),
+                      OutlinedButton(onPressed: (){}, child: Icon(Icons.arrow_back_ios_new_outlined))
+                    ],
+                  ),
                 )
               ];
             } else if (snapshot.hasError) {
@@ -85,7 +106,7 @@ class _ChatScreenState extends State<ChatScreen> {
               ];
             }
             return  Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: children,
               );
           },
