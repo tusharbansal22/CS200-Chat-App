867fab0f74b8e6c53ae890353a3e92e179fe4c4e
tusharb <tusharbansal@iitbhilai.ac.in>
2023-03-10 20:33:39 +0530

    fixed bugs

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -11,6 +11,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   String message="";
+  final messageTextController = TextEditingController();
   final supabase = Supabase.instance.client;
   String email="";
   Future _getData() async{
@@ -38,31 +39,36 @@ class _ChatScreenState extends State<ChatScreen> {
             if (snapshot.hasData) {
               print(snapshot.data.length);
               children = <Widget>[
-                ListView.builder(
-                  reverse: true,
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount:(snapshot.data).length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                          crossAxisAlignment: (snapshot.data[(snapshot.data).length-index-1]['user']!='email')?CrossAxisAlignment.start:CrossAxisAlignment.end,
-                          children: [
-                            Container(
-                                margin:EdgeInsets.symmetric(horizontal: 15,vertical: 2),
-                                child: Text('${snapshot.data[(snapshot.data).length-index-1]['user']}',style: TextStyle(fontSize: 12),)),
-                            Container(
-                              constraints: BoxConstraints(minWidth: 80, maxWidth: 200),
-                              margin:EdgeInsets.symmetric(horizontal: 15,vertical: 2),
-                              padding:EdgeInsets.symmetric(horizontal: 5,vertical: 20),
-                              decoration:BoxDecoration(color: (snapshot.data[(snapshot.data).length-index-1]['user']!='email')?Colors.blue.shade900:Colors.blueGrey.shade400,borderRadius: BorderRadius.circular(10)),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                                child: Text('${snapshot.data[(snapshot.data).length-index-1]['msg']}',style: TextStyle(color: (snapshot.data[(snapshot.data).length-index-1]['user']!='email')?Colors.white:Colors.black),),
-                              ),
-                            )
-                          ],
-                        );
-                    }
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    child: ListView.builder(
+                      reverse: true,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount:(snapshot.data).length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
+                              crossAxisAlignment: (snapshot.data[(snapshot.data).length-index-1]['user']!='${email}')?CrossAxisAlignment.start:CrossAxisAlignment.end,
+                              children: [
+                                Container(
+                                    margin:EdgeInsets.symmetric(horizontal: 15,vertical: 2),
+                                    child: Text('${snapshot.data[(snapshot.data).length-index-1]['user']}',style: TextStyle(fontSize: 12),)),
+                                Container(
+                                  constraints: BoxConstraints(minWidth: 80, maxWidth: 200),
+                                  margin:EdgeInsets.symmetric(horizontal: 15,vertical: 2),
+                                  padding:EdgeInsets.symmetric(horizontal: 5,vertical: 20),
+                                  decoration:BoxDecoration(color: (snapshot.data[(snapshot.data).length-index-1]['user']!='${email}')?Colors.blue.shade900:Colors.blueGrey.shade400,borderRadius: BorderRadius.circular(10)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                                    child: Text('${snapshot.data[(snapshot.data).length-index-1]['msg']}',style: TextStyle(color: (snapshot.data[(snapshot.data).length-index-1]['user']!='${email}')?Colors.white:Colors.black),),
+                                  ),
+                                )
+                              ],
+                            );
+                        }
+                    ),
+                  ),
                 ),
                 Container(
                   alignment: Alignment.bottomCenter,
@@ -71,14 +77,22 @@ class _ChatScreenState extends State<ChatScreen> {
                     children: [
                       Container(
                         width: 250,
-                        child: TextField(onChanged: (change){
-                          message=change;
+                        child: TextField(controller: messageTextController,onChanged: (change){
+
+                          setState(() {
+                            message=change;
+                          });
+
                         },),
                       ),
                       OutlinedButton(onPressed: ()async{
                         await supabase
                             .from('chats')
                             .insert({'user':'${email}','msg':'${message}'});
+                        setState(() {
+                          messageTextController.clear();
+                          message="";
+                        });
                       }, child: Icon(Icons.arrow_back_ios_new_outlined))
                     ],
                   ),
@@ -110,6 +124,7 @@ class _ChatScreenState extends State<ChatScreen> {
               ];
             }
             return  Column(
+              mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: children,
               );
